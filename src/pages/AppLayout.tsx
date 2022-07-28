c22da94ebacf1bcd330af3cd284579592f1f54d8
Ryogo Ito <ryo_go.0907@icloud.com>
2022-07-28 23:10:10 +0900

    共通レイアウトコンポーネントの作成

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import "./AppLayout.css";
 
 interface Props {
   children: React.ReactNode;
+  header: React.ReactNode;
   contentSrc: string;
   contentAlt: string;
 }
 
-export const AppLayout = ({ children, contentSrc, contentAlt }: Props) => {
+export const AppLayout = ({
+  children,
+  contentSrc,
+  contentAlt,
+  header,
+}: Props) => {
   return (
-    <div>
-      <img src={contentSrc} alt={contentAlt} />
-      <main>{children}</main>
+    <div className="contents-ly">
+      <div className="header-ly">{header}</div>
+      <div className="img-ly">
+        {/*  TODO heightについては後々考えて配置する */}
+        <img src={contentSrc} alt={contentAlt} width="100%" height="980" />
+      </div>
+      <div className="main-ly">
+        <main className="main">{children}</main>
+      </div>
     </div>
   );
 };
